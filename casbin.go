dbfc309a0b5ad13c89716dc31dc73470a8156f96
Vasiliy Tolstov <v.tolstov@unistack.org>
2019-09-12 13:10:03 +0300

    fixup
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- casbin.go
+++ casbin.go
@@ -3,37 +3,66 @@
 package casbin
 
 import (
+	"log"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	client "github.com/micro/go-micro/client"
 	casbinpb "github.com/paysuper/casbin-server/casbinpb"
 )
 
+type Logger interface {
+	Printf(string, ...interface{})
+}
+
+type logger struct{}
+
 type (
+	EnforceMode int
 	// Config defines the config for CasbinAuth middleware.
 	Config struct {
 		// Skipper defines a function to skip middleware.
 		Skipper middleware.Skipper
-
+		// Enforce mode
+		Mode EnforceMode
+		// Logger
+		Logger Logger
 		// Casbin micro service.
 		client casbinpb.CasbinService
 	}
 )
 
+const (
+	EnforceModeUnknown EnforceMode = iota
+	EnforceModePermissive
+	EnforceModeEnforcing
+	EnforceModeDisabled
+)
+
 var (
+	DefaultLogger = &logger{}
 	// DefaultConfig is the default CasbinAuth middleware config.
 	DefaultConfig = &Config{
 		Skipper: middleware.DefaultSkipper,
+		Mode:    EnforceModeEnforcing,
+		Logger:  DefaultLogger,
 	}
 )
+
+func (*logger) Printf(fmt string, args ...interface{}) {
+	log.Printf(fmt, args...)
+}
 
 // Middleware returns a CasbinAuth middleware.
 //
 // For valid credentials it calls the next handler.
 // For missing or invalid credentials, it sends "401 - Unauthorized" response.
-func Middleware(c client.Client) echo.MiddlewareFunc {
+func Middleware(c client.Client, mode EnforceMode) echo.MiddlewareFunc {
 	cfg := DefaultConfig
 	cfg.client = casbinpb.NewCasbinService("", c)
+	if mode != EnforceModeUnknown {
+		cfg.Mode = mode
+	}
 	return MiddlewareWithConfig(cfg)
 }
 
@@ -59,13 +88,28 @@ func MiddlewareWithConfig(config *Config) echo.MiddlewareFunc {
 // CheckPermission checks the user/method/path combination from the request.
 // Returns true (permission granted) or false (permission forbidden)
 func (cfg *Config) CheckPermission(c echo.Context) bool {
+	// EnforceModeDisabled return true (permission granted) early
+	if cfg.Mode == EnforceModeDisabled {
+		return true
+	}
+
 	// get user from context
 	user := ""
 	method := c.Request().Method
 	path := c.Request().URL.Path
+
+	// check permisions
 	rsp, err := cfg.client.Enforce(c.Request().Context(), &casbinpb.EnforceRequest{Params: []string{user, path, method}})
-	if err != nil || !rsp.Res {
-		return false
+	if err == nil && rsp.Res {
+		return true
 	}
-	return true
+
+	// EnforceModePermissive log and return true (permission granted)
+	if cfg.Mode == EnforceModePermissive {
+		cfg.Logger.Printf("casbin enforce user:%v path:%v method:%v err:%v rsp:%v", user, path, method, err, rsp.Res)
+		return true
+	}
+
+	// EnforceModeEnforcing return false (permission forbidden)
+	return false
 }
--- go.mod
+++ go.mod
@@ -2,12 +2,18 @@ module github.com/paysuper/echo-casbin-middleware
 
 require (
 	github.com/labstack/echo/v4 v4.1.6
-	github.com/micro/go-micro v1.7.1-0.20190724203029-7ca8f8f0ab98
-	github.com/paysuper/casbin-server v0.0.0-20190809212717-30678425fdda
+	github.com/micro/go-micro v1.10.0
+	github.com/paysuper/casbin-server v0.0.0-20190912100652-3aad9c05777d
+//github.com/paysuper/casbin-server v0.0.0-20190817124042-6b2b224a39ab
 )
 
 replace (
+	github.com/gogo/protobuf => github.com/gogo/protobuf v1.3.0
 	github.com/hashicorp/consul => github.com/hashicorp/consul v1.5.2
 	github.com/hashicorp/consul/api => github.com/hashicorp/consul/api v1.1.0
-	github.com/unistack-org/casbin-micro => github.com/paysuper/casbin-server v0.0.0-20190809212717-30678425fdda
+	github.com/lucas-clemente/quic-go => github.com/lucas-clemente/quic-go v0.11.0
+	gopkg.in/DATA-DOG/go-sqlmock.v1 => github.com/DATA-DOG/go-sqlmock v1.3.3
+//github.com/unistack-org/casbin-micro => github.com/paysuper/casbin-server v0.0.0-20190817124042-6b2b224a39ab
 )
+
+go 1.13
